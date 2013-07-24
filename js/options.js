f7853ae73670f3f7ba37ed1fc7b806fc26712003
michaelsiu <micahel.siu@ideanotion.net>
2013-07-24 19:07:49 -0400

    update with options.js set to ideanotion.net

--- js/options.js
+++ js/options.js
@@ -1,20 +1,19 @@
 (function () {
     ideaPress.options = {
         appTitleImage: null,                      // App title image (approx. 600px x 80px)
-        appTitle: "IdeaPress",                    // App title text
+        appTitle: "IdeaNotion",                    // App title text
         cacheTime: 3600000,                       // Global cache time to try fetch   
-        mainUrl: "http://cbsnewyork.wordpress.com/",         // Main promoting site
-        privacyUrl: "http://cbsnewyork.wordpress.com",      // Privacy URL
+        mainUrl: "http://ideanotion.net",         // Main promoting site
+
+        privacyUrl: "http://ideanotion.net",      // Privacy URL
         fetchOnPostInit: false,                           //only for selfhosted sites
         hubPage : true,
         menu: 'left',
         modules: [
-             { name: wordpresscomModule, options: { id: 1, siteDomain: 'cbsnewyork.wordpress.com', title: "Recent News", typeId: wordpresscomModule.MOSTRECENT, showHub: true, hubSize: 1, templateName: "wpc-tpl-0" } }
-            , { name: wordpresscomModule, options: { id: 2, siteDomain: 'cbsnewyork.wordpress.com', title: 'bookmark', typeId: wordpresscomModule.BOOKMARKS, showHub: true, hubSize: 1, templateName: "wpc-tpl-0" } }
-            , { name: wordpresscomModule, options: { id: 3, siteDomain: 'cbsnewyork.wordpress.com', title: "Sports", typeId: wordpresscomModule.CATEGORY, categoryId: "Sports", showHub: true, hubSize: 2, templateName: "wpc-tpl-0" } }
-            //, { name: wordpresscomModule, options: { id: 4, siteDomain: 'cbsnewyork.wordpress.com', title: "Best Of", typeId: wordpresscomModule.CATEGORY, categoryId: "Best Of", showHub: true, hubSize: 2, templateName: "wpc-tpl-0" } }
-            //, { name: wordpresscomModule, options: { id: 5, siteDomain: 'cbsnewyork.wordpress.com', title: "Travel", typeId: wordpresscomModule.CATEGORY, categoryId: "Travel", showHub: true, hubSize: 2, templateName: "wpc-tpl-0" } }
-        ],
-        searchModule: { name: wordpresscomModule, options: { id: 6, siteDomain: 'cbsnewyork.wordpress.com', title: 'Search', typeId: wordpresscomModule.SEARCH, templateName: "wpc-tpl-0" } }
+     		  { name: wordpressModule, options: { id: 1, siteDomain: 'http://ideanotion.net', title: "Recent News", typeId: wordpressModule.MOSTRECENT, showHub: true, hubSize: 1, templateName: "wp-tpl-0" } }
+            , { name: wordpressModule, options: { id: 3, siteDomain: 'http://ideanotion.net', title: "Recent News", typeId: wordpressModule.MOSTRECENT,  showHub: true, hubSize: 6, templateName: "wp-tpl-0" } }
+            , { name: wordpressModule, options: { id: 4, siteDomain: 'http://ideanotion.net', title: "Open Source", typeId: wordpressModule.CATEGORY, categoryId: 53, showHub: true, hubSize: 6, templateName: "wp-tpl-0" } }
+        ]
+        //,searchModule: { name: wordpresscomModule, options: { id: 6, siteDomain: 'cbsnewyork.wordpress.com', title: 'Search', typeId: wordpresscomModule.SEARCH, templateName: "wpc-tpl-0" } }
     };
 })();
